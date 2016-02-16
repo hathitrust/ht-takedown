b547028f761c48ef1e7f3b15c13a2d9d8f04d8ee
Bryan Hockey <bhock@umich.edu>
2016-02-16 17:38:10 -0500

    cleanup

--- lib/take_down.rb
+++ lib/take_down.rb
@@ -11,42 +11,62 @@ module TakeDown
   def self.path
     @path ||= Pathname.new(__FILE__).parent.parent.realdirpath.to_s
   end
+  
+  
+  def self.get_paths(job)
+    app_list_file = File.join(path, "data/app_list.yml" )
+    output_dir = File.join(job["output_dir"], job["ticket"])
+    access_log_dir = File.join(output_dir, ".access_logs")
+    progress_file = File.join(output_dir, "progress.yml")
+    return progress_file, app_list_file, output_dir, access_log_dir
+  end
+  
+  
+  def self.get_log_dirs(parent_dir, skip_dirs)
+    # Get the folders we need
+    log_dirs = Dir.entries(parent_dir).select { |entry| File.directory? File.join(parent_dir, entry )}
+    log_dirs -= [".", ".."]
+    log_dirs -= (skip_dirs || [])
+    return log_dirs
+  end
+  
+  
+  
+  
 
   def self.execute(job_file)
-    # load files
+    # open job file
     job = YAML.load_file(job_file)
-    app_list = YAML.load_file(File.join(path, "data/app_list.yml" ))[:apps]
-    
-    # create an output directory
-    output_dir = File.join(job["output_dir"], job["ticket"])
+
+    # get paths, make dirs
+    progress_file, app_list_file, output_dir, access_log_dir = get_paths(job)
     `mkdir -p #{output_dir}`
-    access_log_dir = File.join(output_dir, ".access_logs")
     `mkdir -p #{access_log_dir}`
     
-    # detect progress
-    progress_file = File.join(output_dir, "progress.yml")
+    # open configs
+    app_list = YAML.load_file(app_list_file)[:apps]
     progress = YAML.load_file(progress_file)
-
-    # setup the grepper
-    grepper = Grepper.new(app_list, job["volumes"].keys)
-
-    # Get the folders we need
-    log_dirs = Dir.entries(job["parent_dir"]).select { |entry| File.directory? File.join(job["parent_dir"], entry )}
-    log_dirs -= [".", ".."]
-    log_dirs -= (job["skip_dirs"] || [])
-
-    # Begin grepping folders
+    
+    # dirs to read from, dirs to create
+    log_dirs = get_log_dirs(job["parent_dir"], job["skip_dirs"])
+    input_output_map = {}
+    log_dirs.each do |relative_dir| 
+      input_dir = File.join job["parent_dir]"], relative_dir
+      input_output_map[input_dir] = File.join(access_log_dir, "#{relative_dir}-access.log")
+    end
+    
+    
+    # grep!
     unless progress[:grepper]
-      grepped_access_logs = []
-      log_dirs.each do |relative_dir|
-        output_file_path = File.join access_log_dir, "#{relative_dir}-access.log"
-        grepped_access_logs << output_file_path
-        grepper.grep!(File.join(job["parent_dir"], relative_dir), output_file_path)
+      grepper = Grepper.new(app_list, job["volumes"].keys)
+      input_output_map.each do |input_dir, output_file|
+        grepper.grep!(input_dir, output_file)
       end
       progress[:grepper] = true
       File.write(progress_file, progress.to_yaml)
     end
-
+    
+    
     # Load into sql
     db_path = File.join output_dir, ".results.db"
     if progress[:loader]
@@ -55,7 +75,7 @@ module TakeDown
       `rm -f #{db_path}`
       loader = Loader.new(db_path)
       db = loader.get_db
-      grepped_access_logs.each do |grepped_access_log|
+      input_output_map.values.each do |grepped_access_log|
         loader.load!(grepped_access_log)
       end
       progress[:loader] = true
@@ -71,22 +91,29 @@ module TakeDown
       File.write(progress_file, progress.to_yaml)
     end
 
+    
     # Gather report data
-    reporter = Reporter.new(Queryer.new(db))
-    job["volumes"].keys.each do |volume_id|
-      job["volumes"][volume_id].each do |time_window|
-        start_time = time_window["start"]
-        end_time = time_window["end"]
-        reporter.add_volume_to_report(volume_id, start_time, end_time)
+    unless progress[:report]
+      reporter = Reporter.new(Queryer.new(db))
+      job["volumes"].keys.each do |volume_id|
+        job["volumes"][volume_id].each do |time_window|
+          start_time = time_window["start"]
+          end_time = time_window["end"]
+          reporter.add_volume_to_report(volume_id, start_time, end_time)
+        end
       end
+      File.write(File.join(output_dir, "report.txt"), reporter.report)
+      progress[:report] = true
+      File.write(progress_file, progress.to_yaml)
     end
-
-    # Write the report
-    File.write(File.join(output_dir, "report.txt"), reporter.report)
-
+    
+    
     # Cleanup temporary files
     `rm -f #{File.join(access_log_dir, "*")}`
     `rm -f #{db_path}`
   end
+  
+  
+  
 end
 
